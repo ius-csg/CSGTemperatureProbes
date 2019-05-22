0b24a135edfc8379cc99329f20d3524a8bcab885
Zac Clifton <zac16530@gmail.com>
2019-05-22 13:24:15 -0400

    Added comments for testing which port is communicating

--- SerialPortTest.py
+++ SerialPortTest.py
@@ -7,8 +7,10 @@ Created on Tue May 21 16:48:53 2019
 
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-
+# The serial port per machine will be different on windows it will be COM# 
+# and for linux it will be /dev/ttyACM# or /dev/ttyUSB#
+ser = serial.Serial('/dev/ttyACM0', 9600)
+serial.Serial
 while True:
     if(ser.in_waiting >0):
         line = ser.readline()
