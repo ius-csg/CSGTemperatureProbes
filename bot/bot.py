8e682017a37c705fc892bc97707345201a755460
zbouvier <zbouvier99@gmail.com>
2019-09-17 14:25:33 -0400

    removed useless functionality

--- bot/bot.py
+++ bot/bot.py
@@ -63,34 +63,3 @@ client.run(TOKEN)
 
 
 
-
-def doThisbot():
-    with open("C:/Users/thebo/CSGTempuratureProbes/sqlinfo.txt") as f:
-        sqlList = list(f.read().splitlines())
-        sql_user = sqlList[0]
-        sql_password = sqlList[1]   
-    try:
-        connection = mysql.connector.connect(host="192.168.1.101",port=3306, 
-                                            database='Temperature',
-                                            user=sql_user,
-                                            password=sql_password)
-
-        sql_select_Query = "select * from TEMPERATURE_HISTORY"
-        cursor = connection.cursor()
-        cursor.execute(sql_select_Query)
-        records = cursor.fetchall()
-        print("Total number of rows in Laptop is: ", cursor.rowcount)
-
-        print("\nPrinting each laptop record")
-        for row in records:
-            message.channel.send("DATE = ", row[0], )
-            message.channel.send("DEVICE = ", row[1])
-            message.channel.send("TEMPERATURE = ", row[2])
-
-    except Error as e:
-        print("Error reading data from MySQL table", e)
-    finally:
-        if (connection.is_connected()):
-            connection.close()
-            cursor.close()
-            print("MySQL connection is closed")
